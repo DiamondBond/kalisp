0e4b74222180c8bae05ff4e21ec1041c0a441c3f
Diamond Bond <diamond.nigel75@Gmail.com>
2022-08-21 16:12:46 +0530

    implement: basic eval

--- main.c
+++ main.c
@@ -27,6 +27,46 @@ void add_history(char *unused) {}
 #include <editline/readline.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char *op, long y) {
+  if (strcmp(op, "+") == 0) {
+	return x + y;
+  }
+  if (strcmp(op, "-") == 0) {
+	return x - y;
+  }
+  if (strcmp(op, "*") == 0) {
+	return x * y;
+  }
+  if (strcmp(op, "/") == 0) {
+	return x / y;
+  }
+  return 0;
+}
+
+long eval(mpc_ast_t *t) {
+
+  /* If tagged as a number, return it directly. */
+  if (strstr(t->tag, "number")) {
+	return atoi(t->contents);
+  }
+
+  /* The operator is always teh second child. */
+  char *op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+	x = eval_op(x, op, eval(t->children[i]));
+	i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char **argv) {
 
   /* Create parsers */
@@ -46,7 +86,7 @@ int main(int argc, char **argv) {
 			Number, Operator, Expr, Lispy);
 
   /* Print info */
-  puts("Kalisp Version 0.0.3");
+  puts("Kalisp Version 0.0.4");
   puts("Ctrl-C to Exit\n");
 
   /* Main loop */
@@ -58,11 +98,15 @@ int main(int argc, char **argv) {
 	/* Attempt to parse user input */
 	mpc_result_t r;
 	if (mpc_parse("<stdin>", input, Lispy, &r)) {
-	  /* On success print the AST */
-	  mpc_ast_print(r.output);
+	  /* DEBUG: Print AST */
+	  /* mpc_ast_print(r.output); */
+	  /* mpc_ast_delete(r.output); */
+
+	  /* Print out the evaluation results */
+	  long result = eval(r.output);
+	  printf("%li\n", result);
 	  mpc_ast_delete(r.output);
 	} else {
-	  /* Otherwise print error */
 	  mpc_err_print(r.error);
 	  mpc_err_delete(r.error);
 	}
