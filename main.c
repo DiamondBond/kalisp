ef78a00fd165981f9a3e8de38d9f3f6774d04d48
Diamond Bond <diamond.nigel75@Gmail.com>
2022-08-23 01:47:33 +0530

    evaluating errors (1/2)

--- main.c
+++ main.c
@@ -86,34 +86,46 @@ void lval_println(lval v) {
 }
 
 /* Use operator string to see which operation to perform */
-long eval_op(long x, char *op, long y) {
+lval eval_op(lval x, char *op, lval y) {
+  /* If either value is an error return it. */
+  if (x.type == LVAL_ERR) {
+	return x;
+  }
+  if (y.type == LVAL_ERR) {
+	return y;
+  }
+
+  /* Otherwise apply mathematical rules to number values. */
   if (strcmp(op, "+") == 0) {
-	return x + y;
+	return lval_num(x.num + y.num);
   }
   if (strcmp(op, "-") == 0) {
-	return x - y;
+	return lval_num(x.num - y.num);
   }
   if (strcmp(op, "*") == 0) {
-	return x * y;
+	return lval_num(x.num * y.num);
   }
   if (strcmp(op, "/") == 0) {
-	return x / y;
+	/* If second operand is zero return error. */
+	return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
   }
-  return 0;
+  return lval_err(LERR_BAD_OP);
 }
 
-long eval(mpc_ast_t *t) {
+lval eval(mpc_ast_t *t) {
 
-  /* If tagged as a number, return it directly. */
   if (strstr(t->tag, "number")) {
-	return atoi(t->contents);
+	/* Check if there is some error in conversion. */
+	errno = 0;
+	long x = strol(t->contents, NULL, 10);
+	return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
   }
 
   /* The operator is always the second child. */
   char *op = t->children[1]->contents;
 
   /* We store the third child in `x` */
-  long x = eval(t->children[2]);
+  lval x = eval(t->children[2]);
 
   /* Iterate the remaining children and combining. */
   int i = 3;
