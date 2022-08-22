0628208bd0f93e447398642cd0e047147d33a4e3
Diamond Bond <diamond.nigel75@Gmail.com>
2022-08-23 01:41:27 +0530

    setup err infra

--- main.c
+++ main.c
@@ -27,6 +27,64 @@ void add_history(char *unused) {}
 #include <editline/readline.h>
 #endif
 
+/* Create enumeration of possible lval types */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* Create enumeration of possible error types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Declare new lval struct */
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+/* Create a new number type lval */
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+/* Create a new error type lval */
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+/* Print an 'lval' */
+void lval_print(lval v) {
+  switch (v.type) {
+  /* In the case the type is a number print it */
+  /* Then break out of the switch. */
+  case LVAL_NUM:
+	printf("%li", v.num);
+	break;
+	/* Error case */
+  case LVAL_ERR:
+	if (v.err == LERR_DIV_ZERO) {
+	  printf("Error: Division by zero.");
+	}
+	if (v.err == LERR_BAD_OP) {
+	  printf("Error: Invalid operator.");
+	}
+	if (v.err == LERR_BAD_NUM) {
+	  printf("Error: Invalid number.");
+	}
+	break;
+  }
+}
+
+/* Print an 'lval' followed by a newline */
+void lval_println(lval v) {
+  lval_print(v);
+  putchar('\n');
+}
+
 /* Use operator string to see which operation to perform */
 long eval_op(long x, char *op, long y) {
   if (strcmp(op, "+") == 0) {
